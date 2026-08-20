6f20382a1bdbe979f39353ccd9433139808843fa
aiashossain598 <aiashossain598@gmail.com>
2026-08-21 01:59:21 +0600

    Fixed supabase

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,9 +2,9 @@
 // SUPABASE CONFIGURATION
 // ============================================================
 
-const SUPABASE_URL = "https://grhamoekrdzzhkuvkuzc.supabase.co";
+const SUPABASE_URL = "https://cxseqajvzelaelhctucy.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_bufVLrMZq-wA7QFbzEfe6A__DipL78R";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Y5wKXia6JYv0Y7Hau6doxQ_XwItV7wt";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
